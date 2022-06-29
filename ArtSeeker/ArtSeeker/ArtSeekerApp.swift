2963a59086c68e745f427b39142f5d503d08b439
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2022-06-28 21:16:51 -0600

    first commit

---
+++ ArtSeeker/ArtSeeker/ArtSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtSeekerApp.swift
+//  ArtSeeker
+//
+//  Created by Andres Marquez on 2022-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ArtSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
